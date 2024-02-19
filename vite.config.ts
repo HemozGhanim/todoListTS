e0b52b565c2b97ef6f3bcd3212eb1d87d6f0b7c9
ibrahem Ghanim <hemozghanim97@gmail.com>
2024-02-19 18:07:11 +0200

    edit tasks and env

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,27 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), sentryVitePlugin({
-    org: "insurlink",
-    project: "geekbox"
-  })],
+  plugins: [
+    vue(),
+    sentryVitePlugin({
+      org: "insurlink",
+      project: "geekbox",
+    }),
+    sentryVitePlugin({
+      org: "insurlink",
+      project: "geekbox",
+    }),
+  ],
 
   server: {
     port: 3333,
   },
-
+  define: {
+    "process.env.NODE_ENV": JSON.stringify(
+      process.env.NODE_ENV || "development"
+    ),
+  },
   build: {
-    sourcemap: true
-  }
+    sourcemap: true,
+  },
 });
